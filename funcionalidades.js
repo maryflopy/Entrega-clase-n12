4d7cf9cda248f1fd65344e833d8db581a46b9d06
florencia <maryflopy85@hotmail.com.ar>
2024-05-21 22:15:57 -0300

    agregue archivo js

---
+++ funcionalidades.js
@@ -0,0 +1,19 @@
+var nombreIngresado= prompt ("Ingrese su nombre")
+
+function saludo(nombre)
+{
+    alert("Bienvenido " + nombre);   
+}
+var nombreIngresado;
+
+while(nombreIngresado == '')
+{
+ nombreIngresado = prompt("Ingrese su nombre");
+}
+
+saludo(nombreIngresado);
+saludo
+if(nombreIngresado != ""){
+} else {
+        alert("No ingreso su nombre")
+}
